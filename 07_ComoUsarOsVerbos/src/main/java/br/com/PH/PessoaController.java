66322a0173a914d5e7196ed4e64e6b314e4004cc
HenriqueRodrigues971 <kingofsneaker123@gmail.com>
2024-05-17 01:36:43 -0300

    up

--- 07_ComoUsarOsVerbos/src/main/java/br/com/PH/PessoaController.java
+++ 07_ComoUsarOsVerbos/src/main/java/br/com/PH/PessoaController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.PH.Model.Pessoa;
@@ -21,33 +25,32 @@ public class PessoaController {
 	private PessoaServicos servico;
 
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Pessoa> buscarTodos() {
 		return servico.buscarTodos();
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
 	public Pessoa findById(@PathVariable(value = "id") Long id) throws Exception {
 		return servico.buscarPorId(id);
 	}
-	@RequestMapping(
-			method = RequestMethod.POST,
+	@PostMapping(
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Pessoa criarPessoa(@RequestBody Pessoa pessoa) {
 		return servico.criarPessoa(pessoa);
 	}
-	@RequestMapping(
-			method = RequestMethod.PUT,
+	@PutMapping(
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Pessoa atualizarPessoa(@RequestBody Pessoa pessoa) {
 		return servico.atualizarPessoa(pessoa);
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE )
-	public void deletarPessoa(@PathVariable(value = "id") Long id){
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity <?> deletarPessoa(@PathVariable(value = "id") Long id){
 		servico.deletarPessoa(id);
+		return ResponseEntity.noContent().build();
 	}
 
 	/* A anotação PathVariable é usado para recuperar dados da url */
